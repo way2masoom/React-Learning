6578e9fa1913d1b9413133ec30234832d4fd04d5
MD ALAM <alkaserphone@192.168.1.6>
2026-05-22 22:47:56 +0545

    Added Import Export of react components

--- HangManGame/src/App.jsx
+++ HangManGame/src/App.jsx
@@ -1,18 +1,15 @@
 import './App.css'
+import MyButton from './MyButton'
+import { SqureNumber, Addition } from './MyButton'
 
 function App() {
   return (
     <div>
       <h1>Hello React</h1>
-      <Button />
+      <MyButton />
+      <p>Squre of Number = {SqureNumber(5)}</p>
+      <p>Addation of Two number= {Addition(5, 6)}</p>
     </div>
-  )
-}
-
-// Button component
-function Button() {
-  return (
-    <button>Click Me</button>
   )
 }
 
