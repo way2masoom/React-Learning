3c1533f86149d9abe36636c7ad58dc5a938f8cf0
MD ALAM <alkaserphone@192.168.1.6>
2026-05-22 23:03:38 +0545

    Add Alies to SquareNumber

--- 03_ImportExportModule/src/App.jsx
+++ 03_ImportExportModule/src/App.jsx
@@ -4,14 +4,14 @@ import './App.css'
 // import { SqureNumber, Addition } from './MyButton'
 
 // importing button as well as other function in one plac
-import MyButton, { SqureNumber, Addition } from './MyButton'
+import MyButton, { SqureNumber as Sqrt, Addition } from './MyButton'
 
 function App() {
   return (
     <div>
       <h1>Hello React</h1>
       <MyButton />
-      <p>Squre of Number = {SqureNumber(5)}</p>
+      <p>Squre of Number = {Sqrt(5)}</p>
       <p>Addation of Two number= {Addition(5, 6)}</p>
     </div>
   )
