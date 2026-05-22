69e01c8a0eafa13678cb1b29ffc578fb928342bc
MD ALAM <alkaserphone@192.168.1.6>
2026-05-22 21:16:36 +0545

    add react component

---
+++ HangManGame/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Button />
+    </div>
+  )
+}
+
+// Button component
+function Button() {
+  return (
+    <button>Click Me</button>
+  )
+}
+
+export default App
