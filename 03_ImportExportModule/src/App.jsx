e3295e3a8f21d74389dfd5e9d2afdcfc7f20eee1
MD ALAM <alkaserphone@192.168.1.6>
2026-05-22 22:56:38 +0545

    Changed the file name

---
+++ 03_ImportExportModule/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+// import MyButton from './MyButton'
+// import { SqureNumber, Addition } from './MyButton'
+
+// importing button as well as other function in one plac
+import MyButton, { SqureNumber, Addition } from './MyButton'
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <MyButton />
+      <p>Squre of Number = {SqureNumber(5)}</p>
+      <p>Addation of Two number= {Addition(5, 6)}</p>
+    </div>
+  )
+}
+
+export default App
---
+++ 03_ImportExportModule/src/MyButton.jsx
@@ -0,0 +1,16 @@
+// Button component
+function MyButton() {
+    return (
+        <button>Click Me</button>
+    )
+}
+export function SqureNumber(x) {
+    return (x * x)
+}
+export function Addition(x, y) {
+    return (x + y);
+}
+
+export default MyButton;
+
+
