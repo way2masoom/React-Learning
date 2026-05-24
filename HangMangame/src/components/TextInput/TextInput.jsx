396a72851bde315c0d696a951d2b874aff3fd2c1
MD ALAM <alkaserphone@192.168.1.2>
2026-05-24 08:42:44 +0545

    Add StartGame and TextInput components; update App to include new UI elements

---
+++ HangMangame/src/components/TextInput/TextInput.jsx
@@ -0,0 +1,18 @@
+
+// Input component
+function TextInput({ type = "text", label, placeholder = "Enter you TEXT here", onChangeHandler }) {
+    return (
+        <label>
+            <span className="text-gray-700">{label}</span>
+            <input
+                type={type}
+                className="px-4 py-3 m-2 border bg-green-500 text-white rounded-md bg-center w-full"
+                placeholder={placeholder}
+                onChange={onChangeHandler}
+            />
+
+        </label>
+    )
+}
+
+export default TextInput;
---
+++ HangMangame/src/pages/StartGame.jsx
@@ -0,0 +1,20 @@
+// start game component
+
+import Button from "../components/Buttons/Button";
+
+function StartGame() {
+    return (
+        <>
+            <div>
+                <h1 className="text-4xl text-yellow-500 m-5 ">HangMan Game</h1>
+                <Button text="StartGame" styleType="sucess" onClickHandler={() => {
+                    console.log("game LOADED");
+
+                }} />
+                <p className="m-2 text-blue-400">Click on the button to start the game</p>
+            </div>
+        </> /* this called react fragment  */
+    )
+}
+
+export default StartGame;
