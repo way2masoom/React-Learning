0cd383218ac4db922a4ce82b1b262f757133cbfd
MD ALAM <alkaserphone@MacBook-Air.local>
2026-07-11 14:57:33 +0545

    Understanding Reconcillation in react

---
+++ 10_UnderstandingReconciliation/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import './App.css'
+
+
+// Input function
+function Input({ type, placeholder }) {
+  const [s, setS] = useState('');
+  return (
+    <div>
+      <input type={type} placeholder={placeholder} value={s} onChange={(e) => setS(e.target.value)} />
+    </div>
+  )
+}
+
+function App() {
+  const [isStudent, setIsStudent] = useState(false)
+
+  return (
+    <>
+      <form>
+        <Input type='text' placeholder='enter Your name' />
+        <br />
+
+        <input type='checkbox'
+          id='student'
+          name='student'
+          value={isStudent}
+          onChange={() => setIsStudent(!isStudent)}
+        />
+
+        <label htmlFor="student">Are you a student</label>
+        <br />
+
+        {/* {isStudent ? <input type='text' placeholder='Enter your School name' /> :
+          <input type='text' placeholder='Enter your company name' />
+        } */}
+
+        {/* TO SOLVE THIS RECONCILLATION  */}
+
+        {isStudent ? <input type='text' placeholder='Enter your School Name' /> : null}
+
+        {!isStudent ? <input type='text' placeholder='Enter your Company Name' /> : null}
+
+        <br />
+        <button type='submit'>Sumit</button>
+      </form>
+    </>
+  )
+}
+
+export default App
