65d9207c493173568ead1971c81efdc6efc15c15
MD ALAM <alkaserphone@MacBook-Air.local>
2026-07-20 23:08:42 +0545

    React learning part 1

---
+++ 11_Test/Learning/src/App.jsx
@@ -0,0 +1,17 @@
+import Food from "./Food"
+import Fotter from "./Fotter"
+import Header from "./Header"
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Food />
+      <Food />
+      <Fotter />
+    </>
+  )
+}
+
+export default App
---
+++ 11_Test/Learning/src/Fotter.jsx
@@ -0,0 +1,10 @@
+
+function Fotter() {
+    return (
+        <fotter>
+            <p style={{color:"red",textAlign:"center"}}>Copyright &copy; {new Date().getFullYear()} LearningReact.com</p>
+        </fotter>
+    );
+}
+
+export default Fotter;
