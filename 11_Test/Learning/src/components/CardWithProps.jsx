952b148f14a7fca990d3a62f14cddaef07bf21f6
MD ALAM <alkaserphone@MacBook-Air.local>
2026-07-21 07:53:38 +0545

    Card with props

---
+++ 11_Test/Learning/src/components/CardWithProps.jsx
@@ -0,0 +1,11 @@
+function CardWithProps({ title, image, description }) {
+    return (
+        <div className="card">
+            <img src={image} className="CardImage" />
+            <h2 className='cardTitle'>{title}</h2>
+            <p className='cardDes'>{description}</p>
+        </div>
+    );
+}
+
+export default CardWithProps;
